29ee12c4828504e9115d7d5a270418d63e3e768b
Evhenos <bond.evg03@mail.ru>
2023-02-13 17:41:05 +0300

    added dataclass to work with parsing resilts

--- habr_pars.py
+++ habr_pars.py
@@ -2,12 +2,29 @@ import requests
 from bs4 import BeautifulSoup
 import urllib3
 import re
+from dataclasses import dataclass
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) #проверка сертификата
 
+@dataclass
+class HabrsData:
+    """rating: str, link: str, reading_time: str"""
+    rating: str
+    link: str
+    reading_time: str
+    type: str
+    def is_positive(self, percent, vote):
+        nums_in_rating = re.findall(r'\d+', self.rating)
+        nums_in_rating = [int(i) for i in nums_in_rating]
+        positive_percentage = 100 * nums_in_rating[1] / nums_in_rating[0]
 
-def pars_posts(user_habrs):
-    print(user_habrs)
+        if positive_percentage > percent and nums_in_rating[0] > vote:
+            return True
+        else:
+            return False
+
+
+def pars_posts(user_habrs: list[str], user_rercent: int, user_vote: int) -> list[HabrsData]:
     all_habrs = []
     for habr in user_habrs:
         if habr == "all":
@@ -19,26 +36,17 @@ def pars_posts(user_habrs):
         soup = BeautifulSoup(page.content, "html.parser")
         posts = soup.find_all("article", class_="tm-articles-list__item")
 
-
         for post in posts:
-            rating = post.find("div", class_="tm-data-icons").find("div", class_="tm-votes-meter tm-data-icons__item").find("svg", class_="tm-svg-img tm-votes-meter__icon tm-votes-meter__icon tm-votes-meter__icon_appearance-article").find("title").text
+            rating = post.find("div", class_="tm-data-icons").find("div", class_="tm-votes-meter tm-data-icons__item").\
+                          find("svg", class_="tm-svg-img tm-votes-meter__icon tm-votes-meter__icon tm-votes-meter__icon_appearance-article").\
+                          find("title").text
+            link = post.find("a", class_="tm-article-snippet__title-link").get("href")
+            reading_time = post.find('span', class_="tm-article-reading-time__label").text
 
-            nums = re.findall(r'\d+', rating)
-            nums = [int(i) for i in nums]
-            positive_percentage = 100 * nums[1] / nums[0]
+            post_data = HabrsData(rating, "https://habr.com" + link, "Время прочтения:" + reading_time.replace("\n", ""), habr)
 
-            if positive_percentage > 75 and nums[0] > 50:
-
-                link = post.find("a", class_="tm-article-snippet__title-link").get("href")
-                reading_time = post.find('span', class_="tm-article-reading-time__label").text
-
-                all_habrs.append({
-                    "link": "https://habr.com" + link,
-                    "rating": rating,
-                    "reading_time": "Время прочтения:" + reading_time.replace("\n", ""),
-                    "type": habr
-                })
-                print(positive_percentage)
+            if post_data.is_positive(user_rercent, user_vote):
+                all_habrs.append(post_data)
 
     return all_habrs
 
--- handlers/commands.py
+++ handlers/commands.py
@@ -6,7 +6,7 @@ from main import bot, dp, db
 
 from keyboards.web_app import app_keyboard
 from keyboards.new_posts import greet_kb1
-from habr_pars import pars_posts
+from habr_pars import pars_posts, HabrsData
 
 
 @dp.message_handler(Command('start'))
@@ -19,7 +19,9 @@ async def start(message: Message):
 @dp.message_handler(content_types='web_app_data')
 async def buy_process(web_app_message):
     db.change_user(web_app_message.chat.id, web_app_message.web_app_data.data)
-    await bot.send_message(web_app_message.chat.id, "Новые посты", reply_markup=greet_kb1)
+    await bot.send_message(web_app_message.chat.id,
+                           "Новые посты",
+                           reply_markup=greet_kb1)
 
 
 @dp.message_handler(Command('new'))
@@ -32,7 +34,7 @@ async def start(message: Message):
 async def send_posts(chat_id, posts_data):
     for post in posts_data:
         await bot.send_message(chat_id,
-                               text=f"<a href='{post['link']}'>{fmt.hide_link(post['link'])}{post['rating']}\n{post['reading_time']}\n{post['type']}</a>",
+                               text=f"<a href='{post.link}'>{fmt.hide_link(post.link)}{post.rating}\n{post.reading_time}\n{post.type}</a>",
                                parse_mode="HTML")
 
     await bot.send_message(chat_id, "Ваши хабры:", reply_markup=greet_kb1)
